66209def1bd5f7858deaa32864674f734aac2a86
janarthan <jana@email.com>
2020-01-24 18:40:42 +0530

    Init project

---
+++ src/test/java/org/jana/toki/TokiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jana.toki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TokiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
