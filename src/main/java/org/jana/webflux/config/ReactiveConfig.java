b8817ed415aef0b675fd1af01788b2d07cc08797
janarthan <jana@email.com>
2020-01-24 18:51:35 +0530

    Create WebClient bean

--- src/main/java/org/jana/webflux/config/ReactiveConfig.java
+++ src/main/java/org/jana/webflux/config/ReactiveConfig.java
@@ -1,7 +1,25 @@
 package org.jana.webflux.config;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class ReactiveConfig {
+
+    @Value("${api.baseUrl}")
+    String baseUrl;
+
+    @Bean
+    public WebClient webClient() {
+        return WebClient
+                .builder()
+                .baseUrl(baseUrl)
+                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .build();
+    }
+
 }
