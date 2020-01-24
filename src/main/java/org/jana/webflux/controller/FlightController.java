4ee361d1339f7046dcd0bfcd2c0ff2649eaef262
janarthan <jana@email.com>
2020-01-24 18:56:35 +0530

    Add apis get-all-cheap-flights and get-all-business-flights

--- src/main/java/org/jana/webflux/controller/FlightController.java
+++ src/main/java/org/jana/webflux/controller/FlightController.java
@@ -1,12 +1,30 @@
 package org.jana.webflux.controller;
 
+import org.jana.webflux.model.TokiFlight;
+import org.jana.webflux.service.FlightService;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 
 @RestController
 @RequestMapping("/flight")
 public class FlightController {
 
+    private final FlightService flightService;
 
+    public FlightController(FlightService flightService) {
+        this.flightService = flightService;
+    }
+
+    @GetMapping("/cheap/all")
+    public Flux<TokiFlight> getAllCheapFlights() {
+        return flightService.getAllCheapFlights();
+    }
+
+    @GetMapping("/business/all")
+    public Flux<TokiFlight> getAllBusinessFlights() {
+        return flightService.getAllBusinessFlights();
+    }
 
 }
--- src/main/java/org/jana/webflux/service/FlightService.java
+++ src/main/java/org/jana/webflux/service/FlightService.java
@@ -1,11 +1,59 @@
 package org.jana.webflux.service;
 
+import org.jana.webflux.model.BusinessFlightData;
+import org.jana.webflux.model.CheapFlightData;
+import org.jana.webflux.model.FlightType;
+import org.jana.webflux.model.TokiFlight;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Flux;
 
 import java.util.logging.Logger;
 
 @Service
 public class FlightService {
     private static final Logger log = Logger.getLogger(FlightService.class.getName());
+
+    @Value("${api.cheapFlights}")
+    String cheapFlightApi;
+
+    @Value("${api.businessFlights}")
+    String businessFlightApi;
+
+    private final WebClient webClient;
+
+    public FlightService(WebClient webClient) {
+        this.webClient = webClient;
+    }
+
+    public Flux<TokiFlight> getAllCheapFlights() {
+        log.info("Get all cheap flights. Sort them.");
+
+        return webClient
+                .get()
+                .uri(cheapFlightApi)
+                .retrieve()
+                .bodyToMono(CheapFlightData.class)
+                .map(CheapFlightData::getData)
+                .flatMapMany(Flux::fromIterable)
+                .map(e -> new TokiFlight(e.getRoute(), FlightType.CHEAP, e.getDepartureTime(), e.getArrivalTime()))
+                .sort();
+    }
+
+
+    public Flux<TokiFlight> getAllBusinessFlights() {
+        log.info("Get all business flights. Sort them.");
+
+        return webClient
+                .get()
+                .uri(businessFlightApi)
+                .retrieve()
+                .bodyToMono(BusinessFlightData.class)
+                .map(BusinessFlightData::getData)
+                .flatMapMany(Flux::fromIterable)
+                .map(e -> new TokiFlight(e.getDeparture() + "-" + e.getArrival(), FlightType.BUSINESS, e.getArrivalTime(), e.getDepartureTime()))
+                .sort();
+    }
 
 }
